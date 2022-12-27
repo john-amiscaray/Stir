5e8120fded2c9f4b8ac50352b26b2c3927a478a3
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-12-26 19:42:18 -0500

    Added hgroup

--- src/main/java/io/john/amiscaray/stir/domain/elements/HGroup.java
+++ src/main/java/io/john/amiscaray/stir/domain/elements/HGroup.java
@@ -1,10 +1,17 @@
 package io.john.amiscaray.stir.domain.elements;
 
 import io.john.amiscaray.stir.annotation.HTMLElement;
+import lombok.Builder;
+import lombok.Singular;
+
+import java.util.List;
 
 @HTMLElement(tagName = "hgroup")
-public class HGroup {
+public class HGroup extends AbstractElementContainer<AbstractTextElement>{
 
-
+    @Builder
+    public HGroup(String id, @Singular List<String> cssClasses, String style, @Singular List<AbstractTextElement> children) {
+        super(id, cssClasses, style, children);
+    }
 
 }
