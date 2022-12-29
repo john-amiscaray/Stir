9c2d124310c670ea6cc6a38737e142f693387f7a
john-amiscaray <john_ernest_amiscaray@outlook.com>
2022-12-29 12:03:44 -0500

    Added readme up until the Cache support

--- src/main/java/io/john/amiscaray/stir/domain/elements/Anchor.java
+++ src/main/java/io/john/amiscaray/stir/domain/elements/Anchor.java
@@ -5,30 +5,61 @@ import io.john.amiscaray.stir.annotation.HTMLElement;
 import io.john.amiscaray.stir.annotation.InnerContent;
 import lombok.*;
 
+/**
+ * A pojo representing an HTML anchor tag
+ */
 @HTMLElement(tagName = "a")
 @RequiredArgsConstructor
 @AllArgsConstructor
 @Builder
 @ToString
 @EqualsAndHashCode(callSuper = true)
+@Data
 public class Anchor extends AbstractUIElement{
 
+    /**
+     * The inner content of the anchor tag
+     */
     @InnerContent
     private final String label;
+    /**
+     * The href the anchor tag points to
+     */
     @Attribute(name = "href")
     private final String href;
+    /**
+     * The download attribute of the anchor tag
+     */
     @Attribute(name = "download")
     private String download;
+    /**
+     * The hrefLang attribute of the anchor tag
+     */
     @Attribute(name = "hreflang")
     private String hrefLang;
+    /**
+     * The ping attribute of the anchor tag
+     */
     @Attribute(name = "media")
     private String ping;
+    /**
+     * The referrerPolicy attribute of the anchor tag
+     */
     @Attribute(name = "referrerpolicy")
     private String referrerPolicy;
+    /**
+     * The rel attribute of the anchor tag
+     */
     @Attribute(name = "rel")
     private String rel;
+    /**
+     * The target attribute of the anchor tag
+     */
     @Attribute(name = "target")
     private String target;
+    /**
+     * The type attribute of the anchor tag
+     */
     @Attribute(name = "type")
     private String type;
 
