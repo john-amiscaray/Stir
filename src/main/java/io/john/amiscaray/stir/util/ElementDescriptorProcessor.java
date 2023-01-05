d4b0db48f08dbaf744a646dc9c68bb589f99e97d
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-01-04 21:35:18 -0500

    Add attribute descriptors

--- src/main/java/io/john/amiscaray/stir/util/ElementDescriptorProcessor.java
+++ src/main/java/io/john/amiscaray/stir/util/ElementDescriptorProcessor.java
@@ -1,5 +1,6 @@
 package io.john.amiscaray.stir.util;
 
+import io.john.amiscaray.stir.annotation.Attribute;
 import io.john.amiscaray.stir.annotation.HTMLElement;
 import io.john.amiscaray.stir.annotation.exceptions.IllegalElementException;
 import io.john.amiscaray.stir.domain.elements.AbstractUIElement;
@@ -10,6 +11,7 @@ import org.reflections.Reflections;
 import org.reflections.scanners.Scanners;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
@@ -31,8 +33,10 @@ public class ElementDescriptorProcessor {
         Reflections reflections = new Reflections(javaPackage, Scanners.SubTypes);
         Set<Class<? extends AbstractUIElement>> classes = reflections.getSubTypesOf(AbstractUIElement.class);
         // Partitions the descriptor before any brackets used for specifying the attributes, inner content, child elements, etc.
-        String[] partitions = descriptor.split("[\\[({]", 2);
+        String[] partitions = descriptor.split("((?=[\\[({]))", 2);
         String tagNameIdAndClasses = partitions[0];
+        String fieldsDescriptor = partitions.length > 1 ? partitions[1] : "";
+        validateFieldsDescriptor(fieldsDescriptor);
         String tagName = tagNameIdAndClasses.split("[.#]", 2)[0];
         Class<?> elementType = classes.stream()
                 .filter(clazz -> {
@@ -56,7 +60,73 @@ public class ElementDescriptorProcessor {
 
         element.setCssClasses(cssClasses);
         element.setId(id);
+
+        setElementAttributes(fieldsDescriptor, element, elementType);
         return element;
+    }
+
+    private static void validateFieldsDescriptor(String fieldsDescriptor){
+
+        if (fieldsDescriptor.isBlank()) {
+            return;
+        }
+
+        String innerBracketRegex = "([^\\(\\)\\{\\}\\[\\]\\\"\\']*(\\\'.*\\\')?)*";
+
+        if(!fieldsDescriptor.matches("(\\[" + innerBracketRegex + "\\])?(\\(" + innerBracketRegex + "\\))?(\\{" + innerBracketRegex +"\\})?$")){
+            throw new DescriptorFormatException("Malformed element descriptor. After the tag name, css classes, and id, there must be an attribute, inner content, and child descriptor in that order (each optional). Each of these blocks must not have nested brackets that are not enclosed in quotes.");
+        }
+
+    }
+
+    private static void setElementAttributes(String fieldsDescriptor, AbstractUIElement element, Class<?> elementInnerClass) throws IllegalAccessException {
+
+        Pattern attributeDescriptor = Pattern.compile("\\[.*\\]");
+        Matcher attributeMatcher = attributeDescriptor.matcher(fieldsDescriptor);
+
+        while(attributeMatcher.find()){
+
+            String attributeDescriptorStr = attributeMatcher.group();
+            attributeDescriptorStr = attributeDescriptorStr.substring(1, attributeDescriptorStr.length() - 1);
+            if(attributeDescriptorStr.isBlank()){
+                return;
+            }
+            String[] settings = attributeDescriptorStr.split(",");
+            for (String setting : settings) {
+                if (!setting.matches("[^\\s=]+='.+'")) {
+                    throw new DescriptorFormatException("Malformed element descriptor attributes must follow the following regex: [^\\s=]+='.+'");
+                }
+                String[] keyValue = setting.split("=", 2);
+                assert keyValue.length == 2;
+                Field key = ReflectionUtils.getAllFields(elementInnerClass, field -> field.isAnnotationPresent(Attribute.class) && field.getName().equals(keyValue[0])).stream().findFirst()
+                        .orElseThrow(() -> new DescriptorFormatException("Unknown Attribute: " + keyValue[0]));
+                key.setAccessible(true);
+                String value = keyValue[1];
+                value = value.substring(1, value.length() - 1);
+                if(key.getType().equals(Boolean.class)){
+                    if(!value.equalsIgnoreCase("true") && !value.equalsIgnoreCase("false")){
+                        throw new DescriptorFormatException("Boolean attributes must have a value of either true or false");
+                    }
+                    key.set(element, Boolean.parseBoolean(value));
+                }else if(key.getType().equals(Integer.class)){
+                    key.set(element, Integer.parseInt(value));
+                }else if(key.getType().equals(Double.class)){
+                    key.set(element, Double.parseDouble(value));
+                }else if(key.getType().equals(Long.class)){
+                    key.set(element, Long.parseLong(value));
+                }else if(key.getType().equals(Float.class)){
+                    key.set(element, Float.parseFloat(value));
+                }else{
+                    // Assume the thing is a String
+                    if(!key.getType().equals(String.class)){
+                        throw new ElementInitializationException("Unsupported attribute type of: " + key.getType() + " for initialization with descriptors. The following types are supported: Boolean, Integer, Float, Double, Long, String.");
+                    }
+                    key.set(element, value);
+                }
+            }
+
+        }
+
     }
 
     private static List<String> getCSSClasses(String tagIdAndClassesDescriptor){
--- src/test/java/io/john/amiscaray/stir/tests/ElementDescriptorProcessorTest.java
+++ src/test/java/io/john/amiscaray/stir/tests/ElementDescriptorProcessorTest.java
@@ -1,5 +1,6 @@
 package io.john.amiscaray.stir.tests;
 import io.john.amiscaray.stir.domain.elements.Anchor;
+import io.john.amiscaray.stir.util.exceptions.DescriptorFormatException;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.InvocationTargetException;
@@ -37,5 +38,83 @@ public class ElementDescriptorProcessorTest {
                 .build(), element("a#my-link.red.blue.green"));
 
     }
+
+    @Test
+    public void testElementDescriptorWithAttributes() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        assertEquals(Anchor.builder()
+                .id("my-link")
+                .href("https://google.com")
+                .build(), element("a#my-link[href='https://google.com']"));
+
+    }
+
+    @Test
+    public void testElementWithEmptyAttributeDescriptor() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        assertEquals(Anchor.builder()
+                .id("my-link")
+                .build(), element("a#my-link[]"));
+
+    }
+
+    @Test
+    public void testElementWithAttributeDescriptorMultiValues() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        assertEquals(Anchor.builder()
+                .id("my-link")
+                .download("/files/doggo.png")
+                .href("https://google.com")
+                .build(), element("a#my-link[download='/files/doggo.png',href='https://google.com']"));
+
+    }
+
+    @Test
+    public void testElementWithAttributeSelectorValueNotInQuotes() {
+
+        assertThrows(DescriptorFormatException.class, () -> element("a[href=https://google.com]"));
+
+    }
+
+    @Test
+    public void testElementWithAttributeSelectorValueUnclosedQuotes() {
+
+        assertThrows(DescriptorFormatException.class, () -> element("a[href='https://google.com]"));
+
+    }
+
+    @Test
+    public void testElementWithIncorrectQuotesInAttributeSelector() {
+
+        assertThrows(DescriptorFormatException.class, () -> element("a[href=\"https://google.com\"]"));
+
+    }
+
+    @Test
+    public void testElementWithAttributeSelectorWithNestedBrackets() {
+
+        assertThrows(DescriptorFormatException.class, () -> element("a[href(='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href)='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href()='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href{='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href}='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href{}='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href[='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href]='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href[]='']"));
+        assertThrows(DescriptorFormatException.class, () -> element("a[href(){}[]='']"));
+
+    }
+
+    @Test
+    public void testElementWithAttributeSelectorValueHasBrackets() throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        assertEquals(Anchor.builder()
+                .href("(){}[]")
+                .build(), element("a[href='(){}[]']"));
+
+    }
+
+    // TODO expand testing for attribute selector to include values of all the supported types
 
 }
