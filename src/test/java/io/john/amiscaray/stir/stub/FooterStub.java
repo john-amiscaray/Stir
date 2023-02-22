584ce952a10189b728d500e68b205de76a004694
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-02-21 19:12:53 -0500

    Finished testing descriptor aliasing #minor

---
+++ src/test/java/io/john/amiscaray/stir/stub/FooterStub.java
@@ -0,0 +1,8 @@
+package io.john.amiscaray.stir.stub;
+
+import io.john.amiscaray.stir.annotation.HTMLElement;
+import io.john.amiscaray.stir.domain.elements.AbstractUIElement;
+
+@HTMLElement(tagName = "footer")
+public class FooterStub extends AbstractUIElement {
+}
