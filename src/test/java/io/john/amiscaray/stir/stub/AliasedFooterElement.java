f704f67d023bb7948c50daee5e0820aa12beaee6
john-amiscaray <john_ernest_amiscaray@outlook.com>
2023-02-21 19:12:53 -0500

    Finished testing descriptor aliasing

---
+++ src/test/java/io/john/amiscaray/stir/stub/AliasedFooterElement.java
@@ -0,0 +1,8 @@
+package io.john.amiscaray.stir.stub;
+
+import io.john.amiscaray.stir.annotation.HTMLElement;
+import io.john.amiscaray.stir.domain.elements.AbstractUIElement;
+
+@HTMLElement(tagName = "div", alias = "footer")
+public class AliasedFooterElement extends AbstractUIElement {
+}
